e3c37d9ee84f473db1621e8702acb3033b812bfe
Ben Nolan <bnolan@gmail.com>
2015-06-18 14:38:59 +1200

    Added gravatar.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,14 @@
 class Purchase < ActiveRecord::Base
+  scope :feed, -> (lat, long) { order('created_at desc').all }
+  belongs_to :user
+
+  def as_json(*args)
+    attributes.merge({
+      user: { username: user.username, avatar: user.gravatar },
+      venue: { name: 'apple store', address: '567 george st.' },
+      likes: %w/ben mary jane/
+    })
+  end
 
   # def to_json(*args)
   #   result = attributes
@@ -6,18 +16,19 @@ class Purchase < ActiveRecord::Base
   #   if args[:detailed]
   #     result.
 
-  def self.feed(*args)
-    [
-      { name: 'Lamborghini Murcielago', likes: ['mary', 'john', 'dave'], created_at: '2015-06-16T17:47:43+12:00', user: { name: 'ben' }, venue: { name: 'apple store' } }
-      # { name: 'Apple Watch Sport', likes: ['ristari', 'mary', 'dave'], created_at: '2015-06-16T17:47:43+12:00', user: { name: 'captainbenis' }, photo: 'applewatch.jpg', store:
-      #   { name: 'Apple Store, Sydney', address: '367 George Street' }
-      # },
-      # { name: 'Macbook', likes: [], created_at: '2015-06-16T17:47:43+12:00', user: { name: 'captainbenis' }, photo: 'macbook.jpg', store:
-      #   { name: 'Apple Store, Sydney', address: '367 George Street' }
-      # },
-      # { name: 'District personal message bag', likes: ['captainbenis'], created_at: '2015-06-16T17:47:43+12:00', user: { name: 'ristari' }, photo: 'bag.png', store:
-      #   { name: 'Louis Vuitton', address: '365 George St' }
-      # }
-    ]
-  end
+  # def self.feed(*args)
+  #   all
+  #   # [
+  #   #   { name: 'Lamborghini Murcielago', likes: ['mary', 'john', 'dave'], created_at: '2015-06-16T17:47:43+12:00', user: { name: 'ben' }, venue: { name: 'apple store' } }
+  #   #   # { name: 'Apple Watch Sport', likes: ['ristari', 'mary', 'dave'], created_at: '2015-06-16T17:47:43+12:00', user: { name: 'captainbenis' }, photo: 'applewatch.jpg', store:
+  #   #   #   { name: 'Apple Store, Sydney', address: '367 George Street' }
+  #   #   # },
+  #   #   # { name: 'Macbook', likes: [], created_at: '2015-06-16T17:47:43+12:00', user: { name: 'captainbenis' }, photo: 'macbook.jpg', store:
+  #   #   #   { name: 'Apple Store, Sydney', address: '367 George Street' }
+  #   #   # },
+  #   #   # { name: 'District personal message bag', likes: ['captainbenis'], created_at: '2015-06-16T17:47:43+12:00', user: { name: 'ristari' }, photo: 'bag.png', store:
+  #   #   #   { name: 'Louis Vuitton', address: '365 George St' }
+  #   #   # }
+  #   # ]
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ActiveRecord::Base
+  require 'digest/md5'
   devise :database_authenticatable, :recoverable, :validatable
-
   after_create :update_access_token!  
 
   validates :username, presence: true
   validates :email, presence: true
+
+  has_many :purchases
+
+  def gravatar
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email.downcase)
+
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    "http://www.gravatar.com/avatar/#{hash}"
+  end  
 
   private
 
